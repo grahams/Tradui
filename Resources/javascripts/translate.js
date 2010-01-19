31f5d1242ec221f947ba896b9c3b435884758c8c
Brendan G. Lim <brendangl@gmail.com>
2010-01-19 15:13:10 -0500

    Major changes for Android - translation now working but only one word at a time

--- Resources/javascripts/translate.js
+++ Resources/javascripts/translate.js
@@ -1,60 +1,51 @@
+var translateTo = "English to Creole";
+var translateButton;
+var searchField;
+
 function buildDatabase() {
   var db = Titanium.Database.install('db/dictionary.db','db');
   Titanium.App.Properties.setBool("dbInstalled",true);
   db.close();
 }
 
-function buildTranslation(sentence,translateTo) {
-  Titanium.API.info(sentence);
-  Titanium.API.info(translateTo);
-  $("#translate").html("");
-  var wordsArray = sentence.split(" ");
+function buildTranslation(word) {
+  word = word.replace(/^\s*/, "").replace(/\s*$/, ""); // Remove whitespace
   var rows;
-  Titanium.API.info("00");
-  if(wordsArray.length > 0) {
-    Titanium.API.info("0");
-    for(var index in wordsArray) {
-      Titanium.API.info("1");
-      if(wordsArray[index] != '') {
-        Titanium.API.info("2");
-        var db = Titanium.Database.open('db');
-        $("#translate").append("<strong>"+wordsArray[index].toLowerCase()+"</strong>");
-        if(translateTo != null && translateTo == 'Creole to English') {
-          Titanium.API.info("3");
-          rows = db.execute("SELECT DISTINCT english FROM dictionary WHERE creole = '"+wordsArray[index].toLowerCase()+"' AND english != '' ORDER BY english ASC");
-        } else {
-          Titanium.API.info("4");
-          rows = db.execute("SELECT DISTINCT creole FROM dictionary WHERE english = '"+wordsArray[index].toLowerCase()+"' AND creole != '' ORDER BY english ASC ");
-        }
-        Titanium.API.info("5");
-        if(rows != null && rows.getRowCount() > 0) {
-          Titanium.API.info("6");
-        	while (rows.isValidRow()) {
-            Titanium.API.info("7");
-        	  Titanium.API.info(rows.field(0));
-            $("#translate").append("<li style='margin-left:16px;'>"+rows.field(0)+"</li>");
-        		rows.next();
-        	}
-        } else {
-          $("#translate").append("<li style='margin-left:16px;'>No matches</li>");
-        }
-        Titanium.API.info("9");
-        rows.close();        
-        db.close();
-      }
+  if(word != '') {
+    $("#translate").html("");
+    var db = Titanium.Database.install('db/dictionary.db','db');
+    $("#translate").append("<strong>"+word.toLowerCase()+"</strong>");
+    if(translateTo != null && translateTo == 'Creole to English') {
+      rows = db.execute("SELECT DISTINCT english FROM dictionary WHERE creole = '"+word.toLowerCase()+"' AND english != '' ORDER BY english ASC");
+    } else {
+      rows = db.execute("SELECT DISTINCT creole FROM dictionary WHERE english = '"+word.toLowerCase()+"' AND creole != '' ORDER BY english ASC ");
     }
+    if(rows != null && rows.getRowCount() > 0) {
+    	while (rows.isValidRow()) {
+    	  Titanium.API.info(rows.field(0));
+        $("#translate").append("<li style='margin-left:16px;'>"+rows.field(0)+"</li>");
+    		rows.next();
+    	}
+    } else {
+      $("#translate").append("<li style='margin-left:16px;'>No matches</li>");
+    }
+    rows.close();        
+    db.close();
+  } else {
+    Titanium.UI.createAlertDialog({title:'Oops!', 
+			message:'You have to enter in a word for us to be able to translate it.'}).show();
   }
 }
 
 function buildFormElements() {
   var height = (Titanium.Platform.name.indexOf('iPhone') != -1) ? 30: 40;
-	var searchField = Titanium.UI.createTextField({
+	searchField = Titanium.UI.createTextField({
 		id:'searchField',
 		value:'',
 		keyboardType:Titanium.UI.KEYBOARD_ASCII,
 		hintText:'What word do you want to translate?',
 		autocorrect:false,
-		width:278,
+		width:310,
 		fontSize:16,
 		color:'#222',
 		height:height,
@@ -64,7 +55,7 @@ function buildFormElements() {
 	});
 
 
-  var translateButton = Titanium.UI.createButton({
+  translateButton = Titanium.UI.createButton({
   	id:'translateButton',
   	title:'Translate',
   	color:'#ffffff',
@@ -79,7 +70,7 @@ function buildFormElements() {
   
   translateButton.addEventListener("click",function(e) {
     searchField.blur();
-    buildTranslation(searchField.value,$("select#selectBox").val());
+    buildTranslation(searchField.value);
   }); 
 }
 
@@ -90,15 +81,36 @@ window.onload = function() {
     win.open({modal:true});
   });
 	Titanium.UI.currentWindow.setRightNavButton(infoButton);
-	
-  buildFormElements();
   if(Titanium.App.Properties.getBool("dbInstalled") == null || Titanium.App.Properties.getBool("dbInstalled") == 0) {
     buildDatabase();
   }
+	
+  buildFormElements();
+  searchField.blur();
+
+	var menu = Titanium.UI.createMenu();
+
+	menu.addItem("English To Creole", function() {
+    translateTo = "English to Creole";
+    $("#translatingHelper").html("Translating English to Creole");
+		} , null);
+	menu.addItem("Creole To English", function() {
+    translateTo = "Creole to English";
+    $("#translatingHelper").html("Translating Creole to English");
+		}, null);
+	menu.addItem("About Tradui", function() {
+    var win = Titanium.UI.createWindow({url:'/about.html', title:"About", backgroundImage:'../images/tradui_load_screen.png'});
+    win.open({animated:true});
+		}, null);
+
+	Titanium.UI.setMenu(menu);
+
   
   Titanium.UI.currentWindow.addEventListener('focused',function(e) {
 		setTimeout(function(){
       Titanium.UI.currentWindow.setTitle("Tradui");
+      searchField.blur();
+      $("#translatingHelper").focus();
 		},400);
 	});
 }
