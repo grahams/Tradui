1eeb666d534393bdafb9bb924918076e2570ed71
Adam Fletcher <adamfletcher@adam-fletchers-macbook-pro.local>
2010-01-23 15:21:52 -0500

    Added speak button which plays a dummy sound, still has some XXX that needs to be replaced

--- Resources/javascripts/pictographDetails.js
+++ Resources/javascripts/pictographDetails.js
@@ -1,3 +1,31 @@
+function buildFormElements() {
+
+
+
+  var playButton = Titanium.UI.createButton({
+        id:'playButton',
+        title:'Speak',
+        color:'#ffffff',
+        backgroundImage:'/images/BUTT_drk_off.png',
+        backgroundSelectedImage:'/images/BUTT_drk_on.png',
+        backgroundFocusedImage:'/images/BUTT_drk_on.png',
+        height:57,
+        width:300,
+        fontSize:16,
+        fontWeight:'bold'
+  });
+ 
+  playButton.addEventListener("click",function(e) {
+    // XXX we need to get an identifier for the audio, and pass that to play. 
+    // XXX ghall's DB changes are needed, plus we need the pictographKey here.
+    var sound = Titanium.Media.createSound('/audio/creole-female-doctor.mp3');
+    sound.play();
+
+
+  });
+}
+
+
 window.onload = function() {
 var db = Titanium.Database.open('db');
 
@@ -8,4 +36,12 @@ if(pictographKey != null) {
 }
 
 $("#wrapper").show();
+
+buildFormElements();
+
 }
+
+
+
+
+
